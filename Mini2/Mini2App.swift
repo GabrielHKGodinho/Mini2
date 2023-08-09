3085bf819fdeade69212cc65d67b6372c80660b6
Gabriel Godinho <gabrielgodinho@alunos.utfpr.edu.br>
2023-08-09 11:21:43 -0300

    Initial Commit

---
+++ Mini2/Mini2App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini2App.swift
+//  Mini2
+//
+//  Created by Gabriel Henrique Kwiatkovski Godinho on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Mini2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
