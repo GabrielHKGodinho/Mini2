bc7eb7bacdb98acc97e40da75a38576b5d9eedc4
Eduardo Paludo <eduardo.paludo8@gmail.com>
2023-08-23 11:11:19 -0300

    restored main

--- Mini2/ContentView.swift
+++ Mini2/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Teste da gabi!")
+            Text("Hello World!")
         }
         .padding()
     }
