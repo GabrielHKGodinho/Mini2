d68cb85cc62720cd365810b23c82ccc7a26d21c1
Isabela :) <68596490+isabastosj@users.noreply.github.com>
2023-08-28 10:46:00 -0300

    Timer com opções
    Timer com opções de playar, pausar, reiniciar e adicionar + 1 minuto.

--- Mini2/TimerView.swift
+++ Mini2/TimerView.swift
@@ -8,25 +8,82 @@
 import SwiftUI
 
 struct TimerView: View {
-    @State var countDownTimer = 100
-    @State var timerRunning = true
+    @State var countDownTimer = 300
+    @State var timerRunning = false
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     var body: some View {
-        let tempo = countDownTimer
-        let min = tempo / 60
-        let sec = tempo - (min * 60)
-        Text (sec < 10 ? "\(min):0\(sec)" : "\(min):\(sec)")
-            .font(.title)
-            .bold()
-            .padding(.top, 8)
-            .onReceive(timer) { _ in
-                if countDownTimer > 0 && timerRunning {
-                    countDownTimer -= 1
-                } else {
-                    timerRunning = false
+        
+        VStack {
+            
+            Group {
+                let tempo = countDownTimer
+                let min = tempo / 60
+                let sec = tempo - (min * 60)
+                Text (sec < 10 ? "\(min):0\(sec)" : "\(min):\(sec)")
+                    .font(.title)
+                    .bold()
+                    .padding(.top, 8)
+                    .onReceive(timer) { _ in
+                        if countDownTimer > 0 && timerRunning {
+                            countDownTimer -= 1
+                        } else {
+                            timerRunning = false
+                        }
+                    }
+            }
+            
+            HStack(spacing: 16) {
+                Button {
+                    countDownTimer = 300
                     
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 40)
+                            .fill(Color(uiColor: .lightGray))
+                            .frame(width: 80, height: 80)
+                        
+                        Image(systemName: "arrow.counterclockwise")
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                            .font(.title)
+                    }
+                }
+                
+                
+                Button {
+                    timerRunning.toggle()
+                    
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 40)
+                            .fill(Color(uiColor: .lightGray))
+                            .frame(width: 80, height: 80)
+                        
+                        Image(systemName: timerRunning ? "play" : "pause")
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                            .font(.title)
+                    }
+                }
+                
+                Button {
+                    countDownTimer += 60
+                    
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 40)
+                            .fill(Color(uiColor: .lightGray))
+                            .frame(width: 80, height: 80)
+                        
+                        Text("+1m")
+                            .fontWeight(.semibold)
+                            .foregroundColor(.black)
+                            .font(.title)
+                    }
                 }
             }
+        }
+        .padding(32)
     }
 }
 
