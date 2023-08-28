98f1e52984bc0aee6e8761227850d4705ff51539
Gabriel Godinho <gabrielgodinho@alunos.utfpr.edu.br>
2023-08-28 18:11:17 -0300

    inserted "icon" property in the Game class
    inserted "icon" property in the Game class

--- Mini2/Classes/Game.swift
+++ Mini2/Classes/Game.swift
@@ -10,11 +10,13 @@ import Foundation
 class Game {
     var name: String
     var description: String
+    var icon : String
     var categories = [Category]()
     
-    init(name: String, description: String, categories: [Category]) {
+    init(name: String = "Default Name", description: String = "Default Description", icon: String = "defaultIcon", categories: [Category] = []) {
         self.name = name
         self.description = description
+        self.icon = icon
         for category in categories {
             self.categories.append(category)
         }
