c90aa8d35e14317b046f5d06dfbadc962ef96988
Isabela :) <68596490+isabastosj@users.noreply.github.com>
2023-08-30 10:58:19 -0300

    AddPlayers arrumadinho

--- Mini2/Views/AddPlayerView.swift
+++ Mini2/Views/AddPlayerView.swift
@@ -16,75 +16,82 @@ struct AddPlayerView: View {
     @State var isEditing: Bool = false
     
     var body: some View {
-        VStack(alignment: .center, spacing: 16) {
-            Spacer()
-            
-            ForEach(Array(players.enumerated()), id: \.element){index, player in
-                if !(isEditing && player.id == players.last!.id) {
-                    HStack(alignment: .center, spacing: 8) {
-                        Image(systemName: player.icon)
-                            .foregroundColor(.white)
-                        Text(player.name)
-                            .foregroundColor(.white)
-                        Button {
-                            PlayerManager.removePlayer(index: index)
-                            players = PlayerManager.getPlayers()
-                            name = "Player \(PlayerManager.getNumberOfPlayers() + 1)"
-                        } label: {
-                            Image(systemName: "trash.fill")
+        VStack {
+            VStack(alignment: .center, spacing: 16) {
+                Text("Players")
+                    .foregroundColor(.black)
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                Spacer()
+                
+                ForEach(Array(players.enumerated()), id: \.element){index, player in
+                    if !(isEditing && player.id == players.last!.id) {
+                        HStack(alignment: .center, spacing: 8) {
+                            Image(systemName: player.icon)
+                                .foregroundColor(.white)
+                            Text(player.name)
+                                .foregroundColor(.white)
+                            Button {
+                                PlayerManager.removePlayer(index: index)
+                                players = PlayerManager.getPlayers()
+                                name = "Player \(PlayerManager.getNumberOfPlayers() + 1)"
+                            } label: {
+                                Image(systemName: "trash.fill")
+                            }
+                        }
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 8)
+                        .background {
+                            RoundedRectangle(cornerRadius: 24)
+                                .foregroundColor(.black)
                         }
                     }
-                    .padding(.horizontal, 16)
-                    .padding(.vertical, 8)
-                    .background {
-                        RoundedRectangle(cornerRadius: 24)
-                            .foregroundColor(.black)
-                    }
                 }
-            }
-            
-            if isEditing {
-                TextField("", text: $name, prompt: Text("Player \(PlayerManager.getNumberOfPlayers())"))
-                    .onSubmit {
-                        isEditing = false
-                        PlayerManager.setPlayerName(index: PlayerManager.getNumberOfPlayers()-1, name: name)
-                        players = PlayerManager.getPlayers()
-                        name = "Player \(PlayerManager.getNumberOfPlayers() + 1)"
-                    }
-                    .autocorrectionDisabled()
-                    .frame(width: CGFloat(PlayerManager.getLastPlayerName().count + 8) * 5, alignment: .center)
-                    .textFieldStyle(.roundedBorder)
-            }
+                
+                if isEditing {
+                    TextField("", text: $name, prompt: Text("Player \(PlayerManager.getNumberOfPlayers())"))
+                        .onSubmit {
+                            isEditing = false
+                            PlayerManager.setPlayerName(index: PlayerManager.getNumberOfPlayers()-1, name: name)
+                            players = PlayerManager.getPlayers()
+                            name = "Player \(PlayerManager.getNumberOfPlayers() + 1)"
+                        }
+                        .autocorrectionDisabled()
+                        .frame(width: CGFloat(PlayerManager.getLastPlayerName().count + 8) * 5, alignment: .center)
+                        .textFieldStyle(.roundedBorder)
+                }
 
-            Button {
-                PlayerManager.addPlayer(player: Player(name: "Player  \(PlayerManager.getNumberOfPlayers())"))
-                isEditing = true
-                players = PlayerManager.getPlayers()
-            } label: {
-                Text("Adicionar player")
-                    .foregroundColor(.black)
-            }//fim button
-            .buttonStyle(.bordered)
-            
-            Spacer()
-            
-            Button {
-                manager.currentView = .GameListView
-            } label: {
-                ZStack{
-                    RoundedRectangle(cornerRadius: 16)
-                        .foregroundColor(.gray)
-                        .frame(maxWidth: .infinity, maxHeight: 50)
-                    
-                    Text("Next")
-                        .font(.system(size: 20))
-                        .bold()
+                Button {
+                    PlayerManager.addPlayer(player: Player(name: "Player  \(PlayerManager.getNumberOfPlayers())"))
+                    isEditing = true
+                    players = PlayerManager.getPlayers()
+                } label: {
+                    Text("Adicionar player")
                         .foregroundColor(.black)
+                }//fim button
+                .buttonStyle(.bordered)
+                
+                Spacer()
+                
+                Button {
+                    manager.currentView = .GameListView
+                } label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.white)
+                            .frame(height: 70)
+                        
+                        Text("Start")
+                            .foregroundColor(.black)
+                            .font(.title)
+                    }
                 }
-            }//fim button
-            .padding(.bottom, 16)
-        }//fim vstack
-        .padding(.horizontal)
+                .shadow(color: .black.opacity(0.12), radius: 6, x: 10, y: 10)
+            }//fim vstack
+            
+            .padding( 32)
+        }
+        .background(Color(uiColor: .systemGray4))
     }
 }
 
