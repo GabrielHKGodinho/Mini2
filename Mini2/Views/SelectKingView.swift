00a834bf8d36ffd442090c8491a5ae36f174a2d0
Isabela :) <68596490+isabastosj@users.noreply.github.com>
2023-08-30 10:49:42 -0300

    Rules arrumadinho

--- Mini2/Views/SelectKingView.swift
+++ Mini2/Views/SelectKingView.swift
@@ -16,97 +16,115 @@ struct SelectKingView: View {
     
     var body: some View {
         VStack{
-            ZStack {
-                
-                
-                    Text("Select the Master")
-                        .frame(width: 200)
-                        .multilineTextAlignment(.center)
-                        .font(.system(size: 40))
-                        .bold()
-                
-                HStack {
-                    Spacer()
-                    
-                    Button {
-                        playerSelected = Int.random(in: 0..<PlayerManager.getNumberOfPlayers())
-                    } label: {
-                        ZStack{
-                            Circle()
-                                .frame(width: 60)
-                                .foregroundColor(.gray)
-                            
-                            Image(systemName: "arrow.triangle.swap")
-                                .font(.system(size: 26))
-                                .foregroundColor(.white)
-                        }//fim zstack
-                    }//fim button
-                    .padding(.trailing, 12)
-                }//fim hstack
-                .padding(16)
-
-            }
-            
-            ScrollView{
-                ForEach(Array(players.enumerated()), id: \.element){index, player in
-                    Button {
-                        playerSelected = index
-                    } label: {
-                        ZStack {
-                            
-                            HStack{
-                                Circle()
-                                    .frame(width: 60)
-                                    .foregroundColor(.gray)
-                                    .padding(.leading, 16)
-                                
-                                Text(player.name)
+            VStack {
+                ZStack {
+                    ZStack {
+                        Text("Select the Master")
+                            .multilineTextAlignment(.center)
+                            .frame(width: 200)
+                            .foregroundColor(.black)
+                            .font(.largeTitle)
+                            .fontWeight(.semibold)
+                            .padding(.vertical, 40)
+                        
+                        HStack {
+                            Button {
+                                manager.currentView = .RulesView
+                            } label: {
+                                Image(systemName: "chevron.left")
                                     .foregroundColor(.black)
+                                    .font(.title)
                                     .bold()
-                                    .font(.system(size: 20))
-                                    .padding(.leading, 8)
-                                
-                                Spacer()
-                            }
-                            .padding(.vertical, 12)
-//                            .frame(maxWidth: .infinity, maxHeight: 100)
-                            .background(Color(uiColor: .systemGray2))
-                            .cornerRadius(16)
-                            
-                            if playerSelected == index {
-                                RoundedRectangle(cornerRadius: 16)
-                                    .stroke(lineWidth: 6)
-                                    .foregroundColor(.black)
-                                    .cornerRadius(16)
                             }
                             
-                        }//fim zstack
-                    }//fim button
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            Spacer()
+                            
+                            Button {
+                                playerSelected = Int.random(in: 0..<PlayerManager.getNumberOfPlayers())
+                            } label: {
+                                ZStack{
+                                    Circle()
+                                        .frame(width: 60)
+                                        .foregroundColor(.gray)
+                                    
+                                    Image(systemName: "arrow.triangle.swap")
+                                        .font(.system(size: 26))
+                                        .foregroundColor(.white)
+                                }//fim zstack
+                            }//fim button
+                        }//fim hstack
+                    }
+                    
+                    
 
-                }//fim foreach
-            }//fim scroll viem
-            
-            Spacer()
-            
-            Button {
-                manager.currentView = .GameView
-            } label: {
-                HStack{
-                    Text("Start Game")
-                        .foregroundColor(.black.opacity(playerSelected == -1 ? 0.5 : 1))
-                        .bold()
-                        .font(.system(size: 20))
                 }
-                .padding(.vertical, 12)
-                .frame(maxWidth: .infinity, maxHeight: 50)
-                .background(Color(uiColor: .systemGray2).opacity(playerSelected == -1 ? 0.5 : 1))
-                .cornerRadius(16)
-            }//fim button
-            .disabled(playerSelected == -1)
-            .padding(.bottom, 32)
-            
-        }//fim Vstack
-        .padding(.horizontal)
+                
+                ScrollView{
+                    ForEach(Array(players.enumerated()), id: \.element){index, player in
+                        Button {
+                            playerSelected = index
+                        } label: {
+                            ZStack {
+                                
+                                HStack{
+                                    Circle()
+                                        .frame(width: 60)
+                                        .foregroundColor(.gray)
+                                        .padding(.leading, 16)
+                                    
+                                    Text(player.name)
+                                        .foregroundColor(.black)
+                                        .bold()
+                                        .font(.system(size: 20))
+                                        .padding(.leading, 8)
+                                    
+                                    Spacer()
+                                }
+                                .padding(.vertical, 12)
+    //                            .frame(maxWidth: .infinity, maxHeight: 100)
+                                .background(Color(uiColor: .systemGray2))
+                                .cornerRadius(16)
+                                
+                                if playerSelected == index {
+                                    RoundedRectangle(cornerRadius: 16)
+                                        .stroke(lineWidth: 6)
+                                        .foregroundColor(.black)
+                                        .cornerRadius(16)
+                                }
+                                
+                            }//fim zstack
+                        }//fim button
+
+                    }//fim foreach
+                }//fim scroll viem
+                
+                Spacer()
+                
+                Button {
+                    manager.currentView = .GameView
+                } label: {
+                    HStack{
+                        Text("Start Game")
+                            .foregroundColor(.black.opacity(playerSelected == -1 ? 0.5 : 1))
+                            .bold()
+                            .font(.system(size: 20))
+                    }
+                    .padding(.vertical, 12)
+                    .frame(maxWidth: .infinity, maxHeight: 50)
+                    .background(Color(uiColor: .systemGray2).opacity(playerSelected == -1 ? 0.5 : 1))
+                    .cornerRadius(16)
+                }//fim button
+                .padding(.bottom, 32)
+                .disabled(playerSelected == -1)
+                
+            }//fim Vstack
+            .padding(.horizontal, 32)
+        }
+        .background(Color(uiColor: .systemGray4))
     }
 }
 
